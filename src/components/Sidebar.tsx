e71cfba6887fec3127a275eeaff3845af1346623
kovarmiko <kovarmiko@yahoo.com>
2025-08-12 07:42:36 +0200

    adapt sidebar layout

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -5,11 +5,11 @@ interface SidebarProps {
 export default function Sidebar({ isOpen }: SidebarProps) {
   return (
     <aside
-      className={`fixed left-0 top-0 h-full w-64 bg-white shadow transition-transform duration-300 transform ${
-        isOpen ? 'translate-x-0' : '-translate-x-full'
+      className={`${
+        isOpen ? 'w-auto' : 'w-0 truncate'
       }`}
     >
-      {/* Sidebar content goes here */}
+      Sidebar content goes here
     </aside>
   );
 }
