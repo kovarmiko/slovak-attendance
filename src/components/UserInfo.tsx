d21abcbe1cf05bb2fd61b3c071a941f80b94aefc
kovarmiko <kovarmiko@yahoo.com>
2025-08-13 11:14:21 +0200

    improve sizing of text inputs

--- src/components/UserInfo.tsx
+++ src/components/UserInfo.tsx
@@ -19,30 +19,44 @@ export default function UserInfo({
 }: Props) {
   return (
     <div className={classes.userInfo}>
-      <label className={`mr-2 ${classes.printOnly}`}>Meno a&nbsp;priezvisko:</label>
-      <div className='flex flex-col w-full md:w-auto md:mr-2'>
+      <label className={`mr-2 ${classes.printOnly}`}>
+        Meno a&nbsp;priezvisko:
+      </label>
+      <div className='inline-block mr-2'>
         <input
           id='firstName'
-          className={`w-full md:w-auto shadow appearance-none border rounded py-2 px-3 my-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${firstNameError ? 'border-red-500' : ''}`}
+          className={`w-full md:w-auto shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
+            firstNameError ? 'border-red-500' : ''
+          }`}
           value={firstName}
           onChange={(e) => setFirstName(e.target.value)}
           placeholder='Krstné meno'
         />
-        {firstNameError && (
-          <p className='text-red-500 text-xs italic'>Povinné pole</p>
-        )}
+        <p
+          className={`${
+            !firstNameError ? 'invisible' : ''
+          } text-red-500 text-xs italic`}
+        >
+          Povinné pole
+        </p>
       </div>
-      <div className='flex flex-col w-full md:w-auto md:mr-2'>
+      <div className='inline-block mr-2'>
         <input
           id='lastName'
-          className={`w-full md:w-auto shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${lastNameError ? 'border-red-500' : ''}`}
+          className={`w-full md:w-auto shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
+            lastNameError ? 'border-red-500' : ''
+          }`}
           value={lastName}
           onChange={(e) => setLastName(e.target.value)}
           placeholder='Priezvisko'
         />
-        {lastNameError && (
-          <p className='text-red-500 text-xs italic'>Povinné pole</p>
-        )}
+        <p
+          className={`${
+            !lastNameError ? 'invisible' : ''
+          } text-red-500 text-xs italic`}
+        >
+          Povinné pole
+        </p>
       </div>
       <span className={`${classes.printName} ${classes.printOnly}`}>
         {firstName} {lastName}
